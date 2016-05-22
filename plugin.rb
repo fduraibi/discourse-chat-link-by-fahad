db11cbca89430dc7a72c3af779bb1d9308021cea
Fahad Alduraibi <fadnix@gmail.com>
2016-05-22 00:36:36 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: discourse-chat-link-by-fahad
+# about: Adds Aosus Chat link to discourse navigation menu
+# version: 0.1
+# authors: Fahad Alduraibi
+
+register_custom_html(extraNavItem: "<li><a href='https://chat.aosus.org/' target='_blank'>المحادثات</a></li>")
